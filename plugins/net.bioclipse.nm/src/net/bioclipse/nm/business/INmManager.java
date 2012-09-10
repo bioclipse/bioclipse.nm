3dd47e66face58f4f37241f8cb108f87dce12176
Egon Willighagen <egon.willighagen@gmail.com>
2012-09-10 11:46:20 +0200

    Added a basic NM code

--- plugins/net.bioclipse.nm/src/net/bioclipse/nm/business/INmManager.java
+++ plugins/net.bioclipse.nm/src/net/bioclipse/nm/business/INmManager.java
@@ -1,5 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
+/* Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,15 +6,25 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contact: http://www.bioclipse.net/
- ******************************************************************************/
+ */
 package net.bioclipse.nm.business;
 
 import net.bioclipse.core.PublishedClass;
+import net.bioclipse.core.PublishedMethod;
+import net.bioclipse.core.Recorded;
 import net.bioclipse.managers.business.IBioclipseManager;
+import net.bioclipse.nm.domain.Material;
 
 @PublishedClass(
-    value="TODO: Describe the manager here."
+    value="Manager to handle (nano)materials."
 )
 public interface INmManager extends IBioclipseManager {
+
+    @Recorded
+    @PublishedMethod(
+        methodSummary=
+            "Returns the security token for the service."
+    )
+    public Material newMaterial();
 
 }
--- plugins/net.bioclipse.nm/src/net/bioclipse/nm/business/NmManager.java
+++ plugins/net.bioclipse.nm/src/net/bioclipse/nm/business/NmManager.java
@@ -1,5 +1,4 @@
-/*******************************************************************************
- * Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
+/* Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,16 +6,22 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contact: http://www.bioclipse.net/
- ******************************************************************************/
+ */
 package net.bioclipse.nm.business;
 
 import net.bioclipse.managers.business.IBioclipseManager;
+import net.bioclipse.nm.domain.Material;
 
 import org.apache.log4j.Logger;
 
 public class NmManager implements IBioclipseManager {
 
     private static final Logger logger = Logger.getLogger(NmManager.class);
+
+    public Material newMaterial() {
+    	logger.debug("Creating a new domain object implementation...");
+    	return new Material();
+    }
 
     /**
      * Gives a short one word name of the manager used as variable name when
